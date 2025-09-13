ae3dcd2d6dcba2428dadf14a941dea00b171f779
PatelYaksh7 <139530856+jensi2512@users.noreply.github.com>
2025-09-13 17:04:02 +0530

    C7

--- my-website/src/component/Home.jsx
+++ my-website/src/component/Home.jsx
@@ -1,4 +1,4 @@
-import "../static/Home.css"
+import "../Static/Home.css"
 import image1 from "../assets/image1.jpg"
 import image2 from "../assets/image2.jpg"
 import image3 from "../assets/image3.jpg"
